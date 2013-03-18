68c28316af892c6ea5fae30abf513c553f9f734b
Bartosz Firyn <bartosz.firyn@alcatel-lucent.com>
2013-03-18 18:04:40 +0100

    Update OpenIMAJ driver

--- webcam-capture-drivers/webcam-capture-driver-openimaj/src/main/java/com/github/sarxos/webcam/ds/openimaj/OpenImajDriver.java
+++ webcam-capture-drivers/webcam-capture-driver-openimaj/src/main/java/com/github/sarxos/webcam/ds/openimaj/OpenImajDriver.java
@@ -1,13 +1,17 @@
 package com.github.sarxos.webcam.ds.openimaj;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import org.openimaj.video.capture.Device;
 import org.openimaj.video.capture.VideoCapture;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.github.sarxos.webcam.WebcamDevice;
 import com.github.sarxos.webcam.WebcamDriver;
+import com.github.sarxos.webcam.WebcamTask;
 
 
 /**
@@ -17,23 +21,59 @@ import com.github.sarxos.webcam.WebcamDriver;
  */
 public class OpenImajDriver implements WebcamDriver {
 
-	/**
-	 * Cached webcams list.
-	 */
-	private static List<WebcamDevice> webcamDevices = null;
+	static {
+		if (!"true".equals(System.getProperty("webcam.debug"))) {
+			System.setProperty("bridj.quiet", "true");
+		}
+	}
+
+	private static final Logger LOG = LoggerFactory.getLogger(OpenImajDriver.class);
+
+	private static class GetDevicesTask extends WebcamTask {
+
+		private volatile List<WebcamDevice> devices = null;
+
+		public GetDevicesTask(WebcamDriver driver) {
+			super(driver, null);
+		}
+
+		/**
+		 * Return camera devices.
+		 * 
+		 * @param grabber the native grabber to use for search
+		 * @return Camera devices.
+		 */
+		public List<WebcamDevice> getDevices() {
+			try {
+				process();
+			} catch (InterruptedException e) {
+				return Collections.emptyList();
+			}
+			return devices;
+		}
+
+		@Override
+		protected void handle() {
+			devices = new ArrayList<WebcamDevice>();
+			for (Device device : VideoCapture.getVideoDevices()) {
+				devices.add(new OpenImajDevice(device));
+			}
+		}
+	}
 
 	@Override
 	public List<WebcamDevice> getDevices() {
 
-		if (webcamDevices == null) {
-			webcamDevices = new ArrayList<WebcamDevice>();
-			List<Device> devices = VideoCapture.getVideoDevices();
-			for (Device device : devices) {
-				webcamDevices.add(new OpenImajDevice(device));
+		List<WebcamDevice> devices = new GetDevicesTask(this).getDevices();
+
+		if (LOG.isDebugEnabled()) {
+			for (WebcamDevice device : devices) {
+				LOG.debug("OpenIMAJ found device {}", device.getName());
 			}
 		}
 
-		return webcamDevices;
+		return devices;
+
 	}
 
 	@Override
