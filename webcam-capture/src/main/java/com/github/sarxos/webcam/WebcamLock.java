4e1734ad9dd852b739ae481772269cbc0c7bd9f8
Bartosz Firyn <bartosz.firyn@alcatel-lucent.com>
2013-05-30 14:29:28 +0200

    Lock webcam so other Java process cannot use it

--- webcam-capture/src/main/java/com/github/sarxos/webcam/WebcamLock.java
+++ webcam-capture/src/main/java/com/github/sarxos/webcam/WebcamLock.java
@@ -1,5 +1,7 @@
 package com.github.sarxos.webcam;
 
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.prefs.BackingStoreException;
 import java.util.prefs.Preferences;
 
 import org.slf4j.Logger;
@@ -22,12 +24,50 @@ public class WebcamLock {
 	/**
 	 * The preferences instance used to store global variables.
 	 */
-	private static final Preferences PREFS = Preferences.systemNodeForPackage(WebcamLock.class);;
+	private static final Preferences PREFS = Preferences.userNodeForPackage(WebcamLock.class);
+
+	/**
+	 * Update interval (ms).
+	 */
+	private static final long INTERVAL = 2000;
+
+	/**
+	 * Used to update lock state.
+	 * 
+	 * @author sarxos
+	 */
+	private class LockUpdater extends Thread {
+
+		public LockUpdater() {
+			super();
+			setName(String.format("webcam-lock-[%s]", webcam.getName()));
+			setDaemon(true);
+			setUncaughtExceptionHandler(WebcamExceptionHandler.getInstance());
+		}
+
+		@Override
+		public void run() {
+			do {
+				update();
+				try {
+					Thread.sleep(INTERVAL);
+				} catch (InterruptedException e) {
+					LOG.debug("Lock updater has been interrupted");
+					return;
+				}
+			} while (locked.get());
+		}
+
+	}
 
 	/**
 	 * And the Webcam we will be locking.
 	 */
 	private final Webcam webcam;
+
+	private Thread updater = null;
+
+	private AtomicBoolean locked = new AtomicBoolean(false);
 
 	/**
 	 * Creates global webcam lock.
@@ -39,20 +79,50 @@ public class WebcamLock {
 		this.webcam = webcam;
 	}
 
+	private void update() {
+		PREFS.putLong(webcam.getName(), System.currentTimeMillis());
+		try {
+			PREFS.flush();
+		} catch (BackingStoreException e) {
+			LOG.warn("Cannot flush lock preferences", e);
+		}
+	}
+
 	/**
 	 * Lock webcam.
 	 */
 	public void lock() {
+
+		if (isLocked()) {
+			throw new WebcamException(String.format("Webcam %s has been locked and cannot be open", webcam.getName()));
+		}
+
+		if (!locked.compareAndSet(false, true)) {
+			return;
+		}
+
 		LOG.debug("Lock {}", webcam);
-		PREFS.putBoolean(webcam.getName(), true);
+
+		update();
+
+		updater = new LockUpdater();
+		updater.start();
 	}
 
 	/**
 	 * Unlock webcam.
 	 */
 	public void unlock() {
+
+		if (!locked.compareAndSet(true, false)) {
+			return;
+		}
+
 		LOG.debug("Unlock {}", webcam);
-		PREFS.putBoolean(webcam.getName(), false);
+
+		updater.interrupt();
+
+		PREFS.putLong(webcam.getName(), -1);
 	}
 
 	/**
@@ -61,6 +131,32 @@ public class WebcamLock {
 	 * @return True if webcam is locked, false otherwise
 	 */
 	public boolean isLocked() {
-		return PREFS.getBoolean(webcam.getName(), false);
+
+		// check if locked by current process
+
+		if (locked.get()) {
+			return true;
+		}
+
+		// check if locked by other process
+
+		long tsp = PREFS.getLong(webcam.getName(), -1);
+		long now = System.currentTimeMillis();
+
+		LOG.trace("Lock timestamp {} now {} for ", tsp, now, webcam);
+
+		if (tsp > now - INTERVAL * 2) {
+			return true;
+		}
+
+		return false;
+	}
+
+	public static void main(String[] args) {
+		Webcam w = Webcam.getDefault();
+		WebcamLock wl = new WebcamLock(w);
+
+		wl.lock();
+		wl.isLocked();
 	}
 }
