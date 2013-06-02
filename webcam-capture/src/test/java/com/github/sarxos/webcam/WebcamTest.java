82d8ce71ba8a9cc4c232bf4b9a9cb9281b48c6b7
Bartosz Firyn <bartosz.firyn@alcatel-lucent.com>
2013-06-02 12:26:01 +0200

    Latest build is failing on Travis, fixes #108

--- webcam-capture/src/test/java/com/github/sarxos/webcam/WebcamTest.java
+++ webcam-capture/src/test/java/com/github/sarxos/webcam/WebcamTest.java
@@ -29,6 +29,9 @@ public class WebcamTest {
 	@After
 	public void cleanup() {
 		System.out.println(Thread.currentThread().getName() + ": Reset driver");
+		for (Webcam webcam : Webcam.getWebcams()) {
+			webcam.close();
+		}
 		Webcam.resetDriver();
 	}
 
