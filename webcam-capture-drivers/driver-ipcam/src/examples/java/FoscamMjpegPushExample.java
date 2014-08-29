132996c32a45889caad86c6d6e6a44a037cd4655
Bartosz Firyn <bartoszfiryn@gmail.com>
2014-08-29 16:54:34 +0200

    Add examples for PULL/PUSH foscam demo, refs #255

---
+++ webcam-capture-drivers/driver-ipcam/src/examples/java/FoscamMjpegPushExample.java
@@ -0,0 +1,37 @@
+import java.net.MalformedURLException;
+
+import javax.swing.JFrame;
+
+import com.github.sarxos.webcam.Webcam;
+import com.github.sarxos.webcam.WebcamPanel;
+import com.github.sarxos.webcam.ds.ipcam.IpCamAuth;
+import com.github.sarxos.webcam.ds.ipcam.IpCamDeviceRegistry;
+import com.github.sarxos.webcam.ds.ipcam.IpCamDriver;
+import com.github.sarxos.webcam.ds.ipcam.IpCamMode;
+
+
+public class FoscamMjpegPushExample {
+
+	static {
+		Webcam.setDriver(new IpCamDriver());
+	}
+
+	public static void main(String[] args) throws MalformedURLException {
+
+		String name = "Test #255";
+		String url = "http://ce3014.myfoscam.org:20054/videostream.cgi";
+		IpCamMode mode = IpCamMode.PUSH;
+		IpCamAuth auth = new IpCamAuth("user", "user");
+
+		IpCamDeviceRegistry.register(name, url, mode, auth);
+
+		WebcamPanel panel = new WebcamPanel(Webcam.getWebcams().get(0));
+		panel.setFPSLimit(1);
+
+		JFrame f = new JFrame("Test #255 PUSH");
+		f.add(panel);
+		f.pack();
+		f.setVisible(true);
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+}
