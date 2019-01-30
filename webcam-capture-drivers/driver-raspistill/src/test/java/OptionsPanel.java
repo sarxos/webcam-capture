26f5e59b6b0dce7be1c14f68bd335fd46a51e42c
alexmao86 <maoanapex88@163.com>
2019-01-30 15:33:31 +0800

    remove designer loaded jars

--- webcam-capture-drivers/driver-raspistill/src/test/java/OptionsPanel.java
+++ webcam-capture-drivers/driver-raspistill/src/test/java/OptionsPanel.java
@@ -1,4 +1,5 @@
 import java.awt.Component;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.LinkedHashMap;
@@ -16,8 +17,6 @@ import javax.swing.JSlider;
 
 import com.github.sarxos.webcam.Webcam;
 
-import net.miginfocom.swing.MigLayout;
-
 public class OptionsPanel extends JPanel {
 	private static final long serialVersionUID = 1L;
 	private List<Webcam> cams;
@@ -27,147 +26,255 @@ public class OptionsPanel extends JPanel {
 	 */
 	public OptionsPanel() {
 		setToolTipText("colfx ");
-		setLayout(new MigLayout("", "[53px][200px,grow]", "[26px][26px][26px][26px][26px][26px][26px][][][][][][][][][][][][]"));
+		setLayout(new GridLayout(0, 4, 0, 0));
 		
 		JLabel lblWidth = new JLabel("width");
-		add(lblWidth, "cell 0 0,growx,aligny center");
+		add(lblWidth);
 		
 		JSlider slider = new JSlider();
 		slider.setToolTipText("width");
 		slider.setMaximum(1920);
 		slider.setMinimum(196);
-		add(slider, "cell 1 0,alignx left,aligny top");
+		add(slider);
+		
+		JLabel label = new JLabel("");
+		add(label);
+		
+		JLabel label_1 = new JLabel("");
+		add(label_1);
 		
 		JLabel lblHeight = new JLabel("height");
-		add(lblHeight, "cell 0 1,alignx left,aligny center");
+		add(lblHeight);
 		
 		JSlider slider_1 = new JSlider();
 		slider_1.setMaximum(1080);
 		slider_1.setMinimum(128);
 		slider_1.setToolTipText("height");
-		add(slider_1, "cell 1 1,alignx left,aligny top");
+		add(slider_1);
+		
+		JLabel label_2 = new JLabel("");
+		add(label_2);
+		
+		JLabel label_3 = new JLabel("");
+		add(label_3);
 		
 		JLabel lblQuality = new JLabel("quality");
-		add(lblQuality, "cell 0 2,growx,aligny center");
+		add(lblQuality);
 		
 		JSlider slider_2 = new JSlider();
 		slider_2.setToolTipText("quality");
-		add(slider_2, "cell 1 2,alignx left,aligny top");
+		add(slider_2);
+		
+		JLabel label_4 = new JLabel("");
+		add(label_4);
+		
+		JLabel label_5 = new JLabel("");
+		add(label_5);
 		
 		JLabel lblSharpness = new JLabel("sharpness ");
-		add(lblSharpness, "cell 0 3,growx,aligny center");
+		add(lblSharpness);
 		
 		JSlider slider_3 = new JSlider();
 		slider_3.setValue(0);
 		slider_3.setMinimum(-100);
 		slider_3.setToolTipText("sharpness ");
-		add(slider_3, "cell 1 3,alignx left,aligny top");
+		add(slider_3);
+		
+		JLabel label_6 = new JLabel("");
+		add(label_6);
+		
+		JLabel label_7 = new JLabel("");
+		add(label_7);
 		
 		JLabel lblNewLabel = new JLabel("contrast ");
-		add(lblNewLabel, "cell 0 4,growx,aligny center");
+		add(lblNewLabel);
 		
 		JSlider slider_4 = new JSlider();
 		slider_4.setValue(0);
 		slider_4.setToolTipText("contrast");
-		add(slider_4, "cell 1 4,alignx left,aligny top");
+		add(slider_4);
+		
+		JLabel label_8 = new JLabel("");
+		add(label_8);
+		
+		JLabel label_9 = new JLabel("");
+		add(label_9);
 		
 		JLabel lblBrightness = new JLabel("brightness ");
-		add(lblBrightness, "cell 0 5,growx,aligny center");
+		add(lblBrightness);
 		
 		JSlider slider_5 = new JSlider();
 		slider_5.setToolTipText("brightness ");
-		add(slider_5, "cell 1 5,alignx left,aligny top");
+		add(slider_5);
+		
+		JLabel label_10 = new JLabel("");
+		add(label_10);
+		
+		JLabel label_11 = new JLabel("");
+		add(label_11);
 		
 		JLabel lblNewLabel_1 = new JLabel("saturation ");
-		add(lblNewLabel_1, "cell 0 6,growx,aligny center");
+		add(lblNewLabel_1);
 		
 		JSlider slider_6 = new JSlider();
 		slider_6.setToolTipText("saturation ");
-		add(slider_6, "cell 1 6,alignx left,aligny top");
+		add(slider_6);
+		
+		JLabel label_12 = new JLabel("");
+		add(label_12);
+		
+		JLabel label_13 = new JLabel("");
+		add(label_13);
 		
 		JLabel lblIso = new JLabel("ISO ");
-		add(lblIso, "cell 0 7");
+		add(lblIso);
 		
 		JCheckBox chckbxIso = new JCheckBox("ISO ");
 		chckbxIso.setToolTipText("ISO");
-		add(chckbxIso, "cell 1 7");
+		add(chckbxIso);
+		
+		JLabel label_14 = new JLabel("");
+		add(label_14);
+		
+		JLabel label_15 = new JLabel("");
+		add(label_15);
 		
 		JLabel lblVstab = new JLabel("vstab ");
-		add(lblVstab, "cell 0 8");
+		add(lblVstab);
 		
 		JCheckBox chckbxVstab = new JCheckBox("vstab ");
 		chckbxVstab.setToolTipText("vstab ");
-		add(chckbxVstab, "cell 1 8");
+		add(chckbxVstab);
+		
+		JLabel label_16 = new JLabel("");
+		add(label_16);
+		
+		JLabel label_17 = new JLabel("");
+		add(label_17);
 		
 		JLabel lblEv = new JLabel("ev ");
-		add(lblEv, "cell 0 9");
+		add(lblEv);
 		
 		JCheckBox chckbxEv = new JCheckBox("ev ");
 		chckbxEv.setToolTipText("ev ");
-		add(chckbxEv, "cell 1 9");
+		add(chckbxEv);
+		
+		JLabel label_18 = new JLabel("");
+		add(label_18);
+		
+		JLabel label_19 = new JLabel("");
+		add(label_19);
 		
 		JLabel lblExposure = new JLabel("exposure");
-		add(lblExposure, "cell 0 10,alignx trailing");
+		add(lblExposure);
 		
 		JComboBox comboBox = new JComboBox();
 		comboBox.setToolTipText("exposure");
 		comboBox.setModel(new DefaultComboBoxModel(new String[] {"off", "auto", "night", "nightpreview", "backlight", "spotlight", "sports", "snow", "beach", "verylong", "fixedfps", "antishake", "fireworks"}));
-		add(comboBox, "cell 1 10,growx");
+		add(comboBox);
+		
+		JLabel label_20 = new JLabel("");
+		add(label_20);
+		
+		JLabel label_21 = new JLabel("");
+		add(label_21);
 		
 		JLabel lblAwb = new JLabel("awb ");
-		add(lblAwb, "cell 0 11,alignx trailing");
+		add(lblAwb);
 		
 		JComboBox comboBox_1 = new JComboBox();
 		comboBox_1.setModel(new DefaultComboBoxModel(new String[] {"off", "auto", "sun", "cloud", "shade", "tungsten", "fluorescent", "incandescent", "flash", "horizon"}));
 		comboBox_1.setToolTipText("awb");
-		add(comboBox_1, "cell 1 11,growx");
+		add(comboBox_1);
+		
+		JLabel label_22 = new JLabel("");
+		add(label_22);
+		
+		JLabel label_23 = new JLabel("");
+		add(label_23);
 		
 		JLabel lblImxfx = new JLabel("imxfx");
-		add(lblImxfx, "cell 0 12,alignx trailing");
+		add(lblImxfx);
 		
 		JComboBox comboBox_2 = new JComboBox();
 		comboBox_2.setModel(new DefaultComboBoxModel(new String[] {"none", "negative", "solarise", "sketch", "denoise", "emboss", "oilpaint", "hatch", "gpen", "pastel", "watercolour", "film", "blur", "saturation", "colourswap", "washedout", "posterise", "colourpoint", "colourbalance", "cartoon"}));
 		comboBox_2.setToolTipText("imxfx");
-		add(comboBox_2, "cell 1 12,growx");
+		add(comboBox_2);
+		
+		JLabel label_24 = new JLabel("");
+		add(label_24);
+		
+		JLabel label_25 = new JLabel("");
+		add(label_25);
 		
 		JLabel lblColfx = new JLabel("colfx ");
-		add(lblColfx, "cell 0 13,alignx trailing");
+		add(lblColfx);
 		
 		JComboBox comboBox_3 = new JComboBox();
 		comboBox_3.setModel(new DefaultComboBoxModel(new String[] {"U", "V"}));
 		comboBox_3.setToolTipText("colfx ");
-		add(comboBox_3, "cell 1 13,growx");
+		add(comboBox_3);
+		
+		JLabel label_26 = new JLabel("");
+		add(label_26);
+		
+		JLabel label_27 = new JLabel("");
+		add(label_27);
 		
 		JLabel lblMetering = new JLabel("metering ");
-		add(lblMetering, "cell 0 14,alignx trailing");
+		add(lblMetering);
 		
 		JComboBox comboBox_4 = new JComboBox();
 		comboBox_4.setModel(new DefaultComboBoxModel(new String[] {"average", "spot", "backlit", "matrix"}));
 		comboBox_4.setToolTipText("metering ");
-		add(comboBox_4, "cell 1 14,growx");
+		add(comboBox_4);
+		
+		JLabel label_28 = new JLabel("");
+		add(label_28);
+		
+		JLabel label_29 = new JLabel("");
+		add(label_29);
 		
 		JLabel lblHflip = new JLabel("hflip");
-		add(lblHflip, "cell 0 15");
+		add(lblHflip);
 		
 		JCheckBox chckbxHflip = new JCheckBox("hflip");
 		chckbxHflip.setToolTipText("hflip");
-		add(chckbxHflip, "cell 1 15");
+		add(chckbxHflip);
+		
+		JLabel label_30 = new JLabel("");
+		add(label_30);
+		
+		JLabel label_31 = new JLabel("");
+		add(label_31);
 		
 		JLabel lblVflip = new JLabel("vflip ");
-		add(lblVflip, "cell 0 16");
+		add(lblVflip);
 		
 		JCheckBox chckbxVflip = new JCheckBox("vflip ");
 		chckbxVflip.setToolTipText("hflip");
-		add(chckbxVflip, "cell 1 16");
+		add(chckbxVflip);
+		
+		JLabel label_32 = new JLabel("");
+		add(label_32);
+		
+		JLabel label_33 = new JLabel("");
+		add(label_33);
 		
 		JLabel lblRotation = new JLabel("rotation");
-		add(lblRotation, "cell 0 17");
+		add(lblRotation);
 		
 		JSlider slider_7 = new JSlider();
 		slider_7.setValue(0);
 		slider_7.setMaximum(359);
 		slider_7.setToolTipText("rotation ");
-		add(slider_7, "cell 1 17");
+		add(slider_7);
+		
+		JLabel label_34 = new JLabel("");
+		add(label_34);
+		
+		JLabel label_35 = new JLabel("");
+		add(label_35);
 		
 		JButton btnStart = new JButton("Start");
 		btnStart.addActionListener(new ActionListener() {
@@ -179,7 +286,7 @@ public class OptionsPanel extends JPanel {
 				}
 			}
 		});
-		add(btnStart, "cell 0 18");
+		add(btnStart);
 		
 		JButton btnStop = new JButton("Stop");
 		btnStop.addActionListener(new ActionListener() {
@@ -191,7 +298,13 @@ public class OptionsPanel extends JPanel {
 				}
 			}
 		});
-		add(btnStop, "cell 1 18");
+		add(btnStop);
+		
+		JLabel label_36 = new JLabel("");
+		add(label_36);
+		
+		JLabel label_37 = new JLabel("");
+		add(label_37);
 
 	}
 
