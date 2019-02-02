c80dbb4fd9b6fd76552e2b6138924e623e1eb01c
alexmao86 <maoanapex88@163.com>
2019-02-02 12:52:24 +0800

    no value arguments

--- webcam-capture-drivers/driver-raspberrypi/src/main/java/com/github/sarxos/webcam/ds/raspberrypi/OptionsBuilder.java
+++ webcam-capture-drivers/driver-raspberrypi/src/main/java/com/github/sarxos/webcam/ds/raspberrypi/OptionsBuilder.java
@@ -1,7 +1,9 @@
 package com.github.sarxos.webcam.ds.raspberrypi;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
 import org.apache.commons.cli.Option;
@@ -27,8 +29,39 @@ class OptionsBuilder implements Constants{
 	//raspi??? to options
 	private static Map<String, Options> SINGLETONS=new ConcurrentHashMap<>();
 
+	private static Set<String> NO_VALUE_OPTIONS=new HashSet<>();
 	private OptionsBuilder() {
-
+		NO_VALUE_OPTIONS.add(OPT_RAW);
+		NO_VALUE_OPTIONS.add(OPT_LATEST);
+		NO_VALUE_OPTIONS.add(OPT_VERBOSE);
+		NO_VALUE_OPTIONS.add(OPT_DEMO);
+		NO_VALUE_OPTIONS.add(OPT_FULLPREVIEW);
+		NO_VALUE_OPTIONS.add(OPT_KEYPRESS);
+		NO_VALUE_OPTIONS.add(OPT_SIGNAL);
+		NO_VALUE_OPTIONS.add(OPT_GL);
+		NO_VALUE_OPTIONS.add(OPT_GLCAPTURE);
+		NO_VALUE_OPTIONS.add(OPT_SETTINGS);
+		NO_VALUE_OPTIONS.add(OPT_DATETIME);
+		NO_VALUE_OPTIONS.add(OPT_FRAMESTART);
+		
+		NO_VALUE_OPTIONS.add(OPT_PREVIEW);
+		NO_VALUE_OPTIONS.add(OPT_FULLSCREEN);
+		NO_VALUE_OPTIONS.add(OPT_NOPREVIEW);
+		
+		NO_VALUE_OPTIONS.add(OPT_ISO);
+		NO_VALUE_OPTIONS.add(OPT_VSTAB);
+		NO_VALUE_OPTIONS.add(OPT_STATS);
+		NO_VALUE_OPTIONS.add(OPT_STEREO);
+		NO_VALUE_OPTIONS.add(OPT_DECIMATE);
+		NO_VALUE_OPTIONS.add(OPT_HFLIP);
+		NO_VALUE_OPTIONS.add(OPT_VFLIP);
+		NO_VALUE_OPTIONS.add(OPT_3DSWAP);
+		
+		NO_VALUE_OPTIONS.add(OPT_PENC);
+		NO_VALUE_OPTIONS.add(OPT_CIRCULAR);
+		NO_VALUE_OPTIONS.add(OPT_FLUSH);
+		NO_VALUE_OPTIONS.add(OPT_SAVE_PTS);
+		
 	}
 
 	/**
@@ -58,7 +91,9 @@ class OptionsBuilder implements Constants{
 				options.addOption(new Option(opts[0].trim().substring(1), opts[1].trim().substring(2), true, desc));
 			}
 			SINGLETONS.put(name, options);
-			
+			for(String opt:NO_VALUE_OPTIONS) {
+				options.getOption(opt).setArgs(0);
+			}
 			return options;
 		}
 	}
