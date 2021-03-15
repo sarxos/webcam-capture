b303231e71d45d68041fef5a1133f343c7b7829c
alexmao86 <maoanapex88@163.com>
2021-03-15 10:14:28 +0800

    RPI-0002 outline of PICam driver prepration

---
+++ webcam-capture-drivers/driver-raspberrypi/src/main/java/com/github/sarxos/webcam/ds/raspberrypi/PICamDriver.java
@@ -0,0 +1,22 @@
+package com.github.sarxos.webcam.ds.raspberrypi;
+
+import java.util.List;
+
+import com.github.sarxos.webcam.WebcamDevice;
+import com.github.sarxos.webcam.WebcamDriver;
+
+public class PICamDriver implements WebcamDriver{
+
+	@Override
+	public List<WebcamDevice> getDevices() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isThreadSafe() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
